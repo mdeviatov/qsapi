4148dbf7320e17090bc32b610ec5f1a925e6733a
Vermeylen <dvermeylen@fisc.brussels>
2020-04-02 17:30:06 +0200

    Improved explore

--- lib/qlik_methods.py
+++
@@ -1,9 +0,0 @@
-"""
-This script contains Qlik method definitions.
-"""
-
-getAllInfos = dict(handle=1, outkey=-1, id=5, params={}, method='GetAllInfos')
-getDocList = dict(handle=-1, outkey=-1, id=1, params=[], method='GetDocList')
-getScript = dict(handle=+1, outKey=-1, id=3, params={}, method='GetScript')
-openDoc = dict(handle=-1, outkey=-1, id=2, params=[], method='OpenDoc')
-
